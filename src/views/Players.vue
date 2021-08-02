95857897075e9d9be7b832ec6f862f0e77a23731
Mottysuifuryuu <suifuryuu@gmail.com>
2021-08-02 19:03:42 +0900

    add roles

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -9,6 +9,46 @@
     <div class="d-flex justify-center">
       <v-btn class="mr-12" @click="addPlayer()">追加</v-btn>
     </div>
+    <div class="body1 d-flex justify-end">
+      <label style="margin: 24px;">人狼</label>  
+      <input type="number" min=1 max=wolfM v-model.number="wolf" name="wolf" style="border:2px #000000 solid; border-radius: 10px;">
+    </div>
+    <div class="body1 d-flex justify-end">
+      <label style="margin: 24px;">狂人</label>
+      <input type="number" min=0 v-model.number="lunatic" name="lunatic" style="border:2px #000000 solid; border-radius: 10px;">
+    </div>
+  <div class="body2 d-flex justify-end">
+      <label style="margin: 24px;">占い師</label>
+      <input type="number" min=0 v-model.number="caster" name="caster" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">霊媒師</label>
+    <input type="number" min=0 v-model.number="shaman" name="shaman" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">狩人</label>
+    <input type="number" min=0 v-model.number="hunter" name="hunter" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">村人</label>
+    <input type="number" min=0 v-model.number="villager" name="villager" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">共有者</label>
+    <input type="number" min=0 v-model.number="syncronyzer" name="syncronyzer" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">逃亡者</label>
+    <input type="number" min=0 v-model.number="somnumbulist" name="somnumbulist" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body2 d-flex justify-end">
+    <label style="margin: 24px;">追跡者</label>
+    <input type="number" min=0 v-model.number="chaser" name="chaser" style="border:2px #000000 solid; border-radius: 10px;">
+  </div>
+  <div class="body3 d-flex justify-end">
+    <label style="margin: 24px;">妖狐</label>
+    <input type="number" min=0 v-model.number="fox" name="fox" style="border:2px #000000 solid; border-radius: 10px;"/>
+  </div>
   </div>
 </template>
 
@@ -21,6 +61,16 @@ export default Vue.extend({
   data() {
     return {
       players: ['', '', ''],
+      wolf: 1,
+      lunatic: 0,
+      caster: 1,
+      shaman: 0,
+      hunter: 0,
+      villager: 0,
+      syncronyzer: 0,
+      somnumbulist: 0,
+      chaser: 0,
+      fox: 0,
     }
   },
   methods: {
@@ -29,7 +79,7 @@ export default Vue.extend({
     },
     addPlayer() {
       this.players.push('');
-    }
+    },
   },
   mounted() {
     //
@@ -38,5 +88,22 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-
+.body1 {
+  margin: 20px;
+  padding: 5px;
+  background-color: red;
+  font-weight: bolder;
+}
+.body2 {
+  margin: 20px;
+  padding: 5px;
+  background-color: green;
+  font-weight: bolder;
+}
+.body3 {
+  margin: 20px;
+  padding: 5px;
+  background-color: yellow;
+  font-weight: bolder;
+}
 </style>
--- src/views/Rule.vue
+++ src/views/Rule.vue
@@ -3,6 +3,12 @@
     <h1>ルール</h1>
     <v-switch v-model="selfProtection" label="自衛アリ"/>
     <v-switch v-model="sacrifice" label="初日犠牲者アリ(人狼に噛まれる可能性のある役職からランダムに一つが初日犠牲者に割り当てられます)"/>
+    <v-switch v-model="consectiveProtection" label="連続護衛アリ"/>
+    <v-switch v-model="apoptosis" label="共食いアリ"/>
+    <v-switch v-model="lastWill" label="遺言アリ"/>
+    <v-switch v-model="footPrint" label="足音アリ"/>
+    <v-switch v-model="secretBallot" label="秘密投票化"/>
+    <v-switch v-model="randomCast" label="初日ランダム占い(初日夜の占い先は、呪殺が発生せずかつ村人判定が出る人からランダムに選ばれます。)"/>
   </div>
 </template>
 
@@ -15,7 +21,13 @@ export default Vue.extend({
   data() {
     return {
       selfProtection: true,
-      sacrifice: false
+      sacrifice: false,
+      consectiveProtection: true,
+      apoptosis: false,
+      lastWill: false,
+      footPrint: false,
+      secretBallot: false,
+      randomCast: false,
     }
   },
   methods: {
